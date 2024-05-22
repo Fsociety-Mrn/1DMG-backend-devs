b4aa683b80d905297735074c47db23df5b19b577
Art Lisboa <lisboamillen30@gmail.com>
2024-05-22 15:26:42 +0800

    wait lang

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/api', 'API_Endpoints::index');
+$routes->get('/api/posts/create', 'API_Endpoints::create_post');
+$routes->get('/api/posts/read', 'API_Endpoints::read_posts');
+$routes->get('/api/posts/update', 'API_Endpoints::update_posts');
+$routes->get('/api/posts/delete', 'API_Endpoints::delete_post');
